b941e01db88c6a86cbd47e94c0033bdd97bbb754
“EssamQ97” <jaycem.easy@gmail.com>
2026-01-04 21:56:15 +0300

    feat: show spinner when run in progress

--- src/utils/get-workflow-runs.ts
+++ src/utils/get-workflow-runs.ts
@@ -1,24 +1,43 @@
 import { GITHUB_API_BASE } from "../domain/def/url-constants.js";
 import { createRequestError } from "./create-request-error.js";
+import { spinner } from "@clack/prompts";
 import type { TActionsRunsResponse } from "../domain/meta/responses.js";
 
 async function getWorkflowRuns(
   workflowId: number,
   repoPath: string
 ): Promise<TActionsRunsResponse> {
+  let response: TActionsRunsResponse;
+
+  const s = spinner();
+
+  s.start("Fetching workflow runs...");
+
   const workflowsRunsUrl =
     workflowId === -1
       ? `${GITHUB_API_BASE}/repos/${repoPath}/actions/runs`
       : `${GITHUB_API_BASE}/repos/${repoPath}/actions/workflows/${workflowId}/runs`;
 
-  const runsResponse = await fetch(workflowsRunsUrl);
-
-  if (!runsResponse.ok) {
-    throw await createRequestError(runsResponse);
+  while (true) {
+    const runsResponse = await fetch(workflowsRunsUrl);
+    if (!runsResponse.ok) {
+      throw await createRequestError(runsResponse);
+    }
+    response = await runsResponse.json();
+    const inProgressRuns = response.workflow_runs.filter(
+      (run) => run.status === "in_progress" || run.status === "queued"
+    );
+    if (inProgressRuns.length === 0) {
+      break;
+    }
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        s.message(`${inProgressRuns.length} runs in progress...`);
+        resolve(null);
+      }, 5000)
+    );
   }
-
-  const response: TActionsRunsResponse = await runsResponse.json();
-
+  s.stop("Fetching workflow runs...");
   return response;
 }
 
